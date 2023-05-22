b89e3bdaa2774f9b764bb27e5ec0bd9cc8df96a4
SanketWakde71405 <97043168+SanketWakde71405@users.noreply.github.com>
2023-05-22 17:12:24 +0530

    fourth commit

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -33,14 +33,14 @@ const Header = () => {
       <div className='d-flex justify-content-center'>
             <button type="button" className="btn btn-primary cursor-pointer me-2" id="HireButton" data-bs-toggle="modal" data-bs-target="#staticBackdrop" >Hire Me</button>
 
-<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
+<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
         <h5 className="" id="staticBackdropLabel">What project are you looking for?</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
+      <div className="modal-body">
            <div className="d-flex flex-column justify-content-left mb-3">
                   <input className=" w-100 rounded customColor  border border-dark border-opacity-25 p-2 fs-6" type="text" id="name" name="name" placeholder="Name" aria-label="name" required/>
            </div>
